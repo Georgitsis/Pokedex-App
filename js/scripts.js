610b7da88ccc5a4f0ca6a263dc798b42544ba857
PsychoBoyJAck83 <georgitsis.theodoros@gmail.com>
2023-04-25 21:52:42 +0200

    Exercise 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-alert('Hello world');
-let favoriteFood ="Pizza";
-document.write(favoriteFood);
+//Creating variable and assigning it to a blank array
+let pokemonList = []	                                                      
+
+////adding several Pokemon objects to the array
+pokemonList = [                                                               
+	{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},  
+	{ name: 'Pikachu', height: 0.4, types: ['electric']},
+	{ name: 'Weedle', height: 0.3, types: ['bug', 'poison']},
+	{ name: 'Onix', height: 8.8, types: ['rock', 'ground']}
+];
