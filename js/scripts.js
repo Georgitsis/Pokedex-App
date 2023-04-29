3a42fdf81fdeae5959400bea43ee686f621befdb
PsychoBoyJAck83 <georgitsis.theodoros@gmail.com>
2023-04-29 12:46:03 +0200

    write all pokemon names and height on DOM +<br>

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,11 @@ pokemonList = [
 	{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},  
 	{ name: 'Pikachu', height: 0.4, types: ['electric']},
 	{ name: 'Weedle', height: 0.3, types: ['bug', 'poison']},
-	{ name: 'Onix', height: 8.8, types: ['rock', 'ground']}
+	{ name: 'Onix', height: 8.8, types: ['rock', 'ground']},
+	{ name: 'Drapion', height: 1.3, types: ['poison','dark']}
 ];
+
+for (let i=0;i<pokemonList.length;i++){
+	//document.write(pokemonList[i].name + " " + pokemonList[i].height + "<br>");
+	document.write(`${pokemonList[i].name} ${pokemonList[i].height}<br>`);
+}
