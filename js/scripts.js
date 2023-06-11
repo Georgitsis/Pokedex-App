d65f04a2b979d816f88092f7cb0e2d863c2db98c
PsychoBoyJAck83 <georgitsis.theodoros@gmail.com>
2023-06-11 18:57:48 +0200

    Comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,34 @@
 //Nesting PokemonList in a IIFE
 let pokemonRepository = (function () {
 	let pokemonList = [];
-	let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=1273";
+	let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=1273";	//url of the pokemon API
 
-/* returns the entire pokemon list*/
+// returns the entire pokemon list
 function getAll() {
 	  return pokemonList;
 }
 
-/* If item is a valid pokemon object it will be added to the pokemon list*/
+// If item is a valid pokemon object it will be added to the pokemon list
 function add(item) {
 	if(isPokemon(item))
 		pokemonList.push(item);
+	else
+		console.log("One of the items was not a pokemon!");
 }
 
-/* expects a string as parameter and searches the pokemon list for an exact name match. Returns an array of objects if several matches */ 
-function findPokemonByName(pokemonName){
-	let results = pokemonList.filter(pokemon => pokemon.name === pokemonName);
-	return results;
-}
-
-/* Expects a pokemon object as parameter. Will create a list item for the pokemon */
+// Expects a pokemon object as parameter. Will create a list item for the pokemon
 function addListItem(pokemon) {
 	let ListElement = document.createElement('li');						//create a new list element
-	ListElement.classList.add('pokemon-list-item',
+	ListElement.classList.add('pokemon-list-item',						//Adding necessary classes to the list element
 	'col-12','col-sm-6','col-md-4','col-lg-3','list-group-item');
 	let pokemonButton = document.createElement('button');				//create a new pokemon button
 	pokemonButton.classList.add('btn-block','pokemon-button');		//add pokemonButton classes 
 	pokemonButton.setAttribute("data-toggle","modal");					//set data-toggle attribute to modal
 	pokemonButton.setAttribute("data-target","#pokemonModal");		//set data-target attribute to pokemonModal
 	addPokemonButtonEvent(pokemonButton,pokemon);						//adds a click event to the pokemon button
-	pokemonButton.innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);	//Set inner text of button to the pokemon name, capitalize first letter
+	pokemonButton.innerText = pokemon.name.charAt(0).toUpperCase()	//Set inner text of button to the pokemon name, capitalize first letter
+	 + pokemon.name.slice(1);	
 	let pokemonList = document.querySelector("ul.pokemon-list");	//Select the ul element with pokemon-list class
-	//let buttonGrid = document.querySelector(".button-grid");
 	pokemonList.appendChild(ListElement);									//add a list item to ul.pokemon.list
 	ListElement.appendChild(pokemonButton); 								//add a button to the list element
 }
@@ -41,7 +37,7 @@ function addListItem(pokemon) {
 function showDetails(pokemon){
 	loadDetails(pokemon).then(function(){
 
-//naming modal elements for convenience
+		//naming modal elements for convenience
 		let modalHeader = document.querySelector(".modal-title");
 		let modalHeight = document.querySelector(".modal-height");
 		let modalWeight = document.querySelector(".modal-weight");
@@ -109,7 +105,8 @@ function loadList() {
 	})
  }
 
-//loads a pokemon details by using the url saved in the pokemon object 
+//loads a pokemon details by using the url saved in the pokemon object
+
 function loadDetails(item) {
 	let url = item.detailsUrl;
 	return fetch(url).then(function (response) {
@@ -129,7 +126,6 @@ function loadDetails(item) {
 	return {
 	  	add : add,
 	  	getAll : getAll,
-	  	findPokemonByName : findPokemonByName,
 	  	addListItem : addListItem,
 	  	showDetails : showDetails,
 	  	addPokemonButtonEvent : addPokemonButtonEvent,
@@ -140,8 +136,9 @@ function loadDetails(item) {
 
 /**********************************************************************Function declarations*******************************************/
 
-// isPokemon() : Will return true if item is a "pokemon" by checking that it is an object, checking that it has the correct object keys and checking that the data stored in the object keys has the correct type.
-// Otherwise it will return false.
+// isPokemon() : Will return true if item is a "pokemon" by checking that it is an object,
+// checking that it has the correct object keys and checking that the data stored in the object
+// keys has the correct type. Otherwise it will return false.
 
 function isPokemon(item){
 	let correctObjectKeys = ["name","detailsUrl"];
